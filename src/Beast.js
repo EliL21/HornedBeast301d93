57c696a7cc18bf2c07fa89f76d255528f3da5cc0
eli bailey <baileyeli19@gmail.com>
2023-01-11 00:49:21 +0000

    day-two

--- src/Beast.js
+++ src/Beast.js
@@ -1,15 +1,42 @@
 import React from "react";
+import './Beast.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+
 
 
 class Beast extends React.Component {
-  render(){
-    return(
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite: 0,
+    }
+  }
+
+  handleFavorite = () => {
+    this.setState({
+      favorite: this.state.favorite + 1
+    })
+  }
+
+  render() {
+    return (
       <>
-      <article>
-        <p>{this.props.title}</p>
-        <p>{this.props.description}</p>
-      </article>
       
+      
+        <Card style={{ width: '18rem' }}>
+          <p>{this.props.title}</p>
+          <p>💙{this.state.favorite}Favorite</p>
+          <p onClick={this.handleFavorite}>Click for favorite!</p>
+          <Card.Img src={this.props.image_url} alt={this.props.description} />
+          <p>{this.props.description}</p>
+          <Button>Favorite</Button>
+
+        </Card>
+        
+
       </>
     )
   }
